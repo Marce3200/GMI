1390fd458fa3ab8206fb0cde0983e763b12838db
antoniaarga <maargandonaa@gmail.com>
2022-12-01 08:00:00 -0300

    footer y arreglo responsive

--- src/componentes/QuienesSomos/QuienesSomos.js
+++ src/componentes/QuienesSomos/QuienesSomos.js
@@ -8,72 +8,72 @@ import Card from "react-bootstrap/Card";
 const QuienesSomos = () => {
   return (
     <Container fluid className="seccion-quienes">
-      <Row>
-        <center>
+      <center>
+        <Row className="w-75">
           <Col>
             <p className="titulo-seccion-quienes">Quiénes Somos</p>
 
             <p className="texto-descripcion">
               MGI Gestión Inmobiliaria nace a partir de la unión de competencias
               entre profesionales provenientes del área legal con profesionales
-              provenientes de la administración de empresas.
-              <br />
-              <br />
-              La experiencia acumulada nos llevó a dar un paso adelante de la
-              asesoría técnica, para convertirnos en Gestores Inmobiliarios.
-              Poniendo a disposición de nuestros clientes todas nuestras
-              competencias. Nuestra experiencia nos permite comprender que el
-              éxito de la Gestión Inmobiliaria se basa no sólo en una acabada
-              asesoría técnica, sino también en entender las necesidades
-              particulares de cada uno de nuestros clientes.
-              <br />
-              <br />
-              Por eso consideramos que la comunicación es un pilar fundamental
-              en las relaciones que generamos.
+              provenientes de la administración de empresas. La experiencia
+              acumulada nos llevó a dar un paso adelante de la asesoría técnica,
+              para convertirnos en Gestores Inmobiliarios. Poniendo a
+              disposición de nuestros clientes todas nuestras competencias.
+              Nuestra experiencia nos permite comprender que el éxito de la
+              Gestión Inmobiliaria se basa no sólo en una acabada asesoría
+              técnica, sino también en entender las necesidades particulares de
+              cada uno de nuestros clientes. Por eso consideramos que la
+              comunicación es un pilar fundamental en las relaciones que
+              generamos.
             </p>
           </Col>
-          </center>
-      </Row>
+        </Row>
+      </center>
       <center>
-      <Row>
-        
-        <Col sm={12} md={4}>
-          <Card className="card-mision" >
-            {/* <Card.Img className="foto-menu" variant="top" src={SUSHI1} /> */}
-            <Card.Body>
-              <Card.Title className="titulo-valores text-center">Misión</Card.Title>
-              <Card.Text className="text-valores text-center">
-                Generar valor de forma eficaz y responsable para quienes nos
-                confíen sus inmuebles.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col sm={12} md={4}>
-          <Card className="card-mision" >
-            {/* <Card.Img className="foto-menu" variant="top" src={sushi2} /> */}
-            <Card.Body>
-              <Card.Title className="titulo-valores text-center">Visión</Card.Title>
-              <Card.Text className="text-valores text-center">
-                Entregar un servicio de alta calidad técnica, eficiente y
-                transparente.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col sm={12} md={4}>
-          <Card className="card-mision" >
-            {/* <Card.Img className="foto-menu" variant="top" src={sushi6} /> */}
-            <Card.Body>
-              <Card.Title className="titulo-valores text-center">Valores</Card.Title>
-              <Card.Text className="text-valores text-center">
-                Compromiso, responsabilidad y gratitud a nuestros clientes.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        
-      </Row> 
+        <Row>
+          <Col sm={12} md={4}>
+            <Card className="card-mision mt-3">
+              {/* <Card.Img className="foto-menu" variant="top" src={SUSHI1} /> */}
+              <Card.Body>
+                <Card.Title className="titulo-valores text-center">
+                  Misión
+                </Card.Title>
+                <Card.Text className="text-valores text-center">
+                  Generar valor de forma eficaz y responsable para quienes nos
+                  confíen sus inmuebles.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col sm={12} md={4}>
+            <Card className="card-mision mt-3">
+              {/* <Card.Img className="foto-menu" variant="top" src={sushi2} /> */}
+              <Card.Body>
+                <Card.Title className="titulo-valores text-center">
+                  Visión
+                </Card.Title>
+                <Card.Text className="text-valores text-center">
+                  Entregar un servicio de alta calidad técnica, eficiente y
+                  transparente.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col sm={12} md={4}>
+            <Card className="card-mision mt-3">
+              {/* <Card.Img className="foto-menu" variant="top" src={sushi6} /> */}
+              <Card.Body>
+                <Card.Title className="titulo-valores text-center">
+                  Valores
+                </Card.Title>
+                <Card.Text className="text-valores text-center">
+                  Compromiso, responsabilidad y gratitud a nuestros clientes.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </center>
     </Container>
   );
